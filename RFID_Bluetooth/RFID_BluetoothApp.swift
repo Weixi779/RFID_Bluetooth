a154200d08a6820e513c7c30c64c9309c0ee1f02
Weixi779 <1349750521@qq.com>
2020-11-24 11:46:00 +0800

    Initial Commit

---
+++ RFID_Bluetooth/RFID_BluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  RFID_BluetoothApp.swift
+//  RFID_Bluetooth
+//
+//  Created by 孙世伟 on 2020/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RFID_BluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
